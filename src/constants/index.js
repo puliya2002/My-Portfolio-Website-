f32e8e3ae77cf8f654079b6e0ea792221967ac50
Pulindu Vidmal <vidmalpulindu@gmail.com>
2025-01-26 17:43:04 +0530

    new project

--- src/constants/index.js
+++ src/constants/index.js
@@ -14,7 +14,12 @@ import Riverside from "../assets/riverside.webp";
 import RiversideSS from "../assets/riversidess.webp";
 import PortfolioSS from "../assets/portfolioss.webp";
 import Portfolio from "../assets/portfolio.webp";
-
+import Trust from "../assets/trust.webp";
+import TrustSS from "../assets/trustss.webp";
+import Store from "../assets/store&snore.webp";
+import StoreSS from "../assets/store&snoress.webp";
+import Seen from "../assets/seen.webp";
+import SeenSS from "../assets/seenss.webp";
 
 export const ProjectList = [
   {
@@ -25,98 +30,126 @@ export const ProjectList = [
     ss: Appss,
     giturl: "https://github.com/puliya2002/money_track.git",
     weburl: "",
-    description: "As part of a second-year university group project, We developed MoneyTrack, an expense tracking application using the Flutter framework and integrated with a Firebase database. The goal of MoneyTrack is to provide a user-friendly platform for recording and monitoring expenses efficiently. This project showcases my skills in teamwork, mobile app development, database integration, and creating intuitive user interfaces."
-    
+    description:
+      "As part of a second-year university group project, We developed MoneyTrack, an expense tracking application using the Flutter framework and integrated with a Firebase database. The goal of MoneyTrack is to provide a user-friendly platform for recording and monitoring expenses efficiently. This project showcases my skills in teamwork, mobile app development, database integration, and creating intuitive user interfaces.",
   },
   {
     id: 2,
+    title: "Sovereign Trust",
+    image: Trust,
+    skills: ["WordpPress", "Woocommerce", "Elementor", "JavaScript", "CSS"],
+    ss: TrustSS,
+    giturl: "",
+    weburl: "https://sovereigntrustco.com/",
+    description:
+      "Project for a client based in the USA who runs a trust-based business. Wordpress-based project. Logo and all branding designed by me.",
+  },
+
+  {
+    id: 3,
     title: "Green Supermarket",
     image: GreenSupper,
     skills: ["HTML", "CSS", "JavaScript", "JAVA", "Git", "SQL"],
     ss: GreenSS,
     giturl: "https://github.com/puliya2002/GreenSuperMarket.git",
     weburl: "",
-    description: "University second-year group project involved developing an e-commerce web application tailored for a supermarket We used HTML, CSS, and JavaScript as front-end technologies and JAVA for the backend. Integrated PayPal sandbox as the payment gateway."
-    
+    description:
+      "University second-year group project involved developing an e-commerce web application tailored for a supermarket We used HTML, CSS, and JavaScript as front-end technologies and JAVA for the backend. Integrated PayPal sandbox as the payment gateway.",
   },
 
   {
-    id: 3,
+    id: 4,
+    title: "Store and Snore Nashville",
+    image: Store,
+    skills: ["WordPress", "Woocommerce", "Elementor", "Paypal", "Stripe"],
+    ss: StoreSS,
+    giturl: "",
+    weburl: "https://storeandsnorenashville.com/",
+    description:
+      "Store & Snore is a luggage booking website designed to provide seamless booking experiences for travelers. The website was built on WordPress with WooCommerce and Elementor for a user-friendly interface. I integrated the RnD Booking plugin to handle the booking system efficiently. The payment system includes Stripe and PayPal gateways for secure transactions. The UI design was meticulously planned and created using Figma to ensure a clean and intuitive user experience.",
+  },
+
+  {
+    id: 5,
+    title: "SEEN Org",
+    image: Seen,
+    skills: ["WordPress", "Woocommerce", "CSS", "Paypal", "Figma"],
+    ss: SeenSS,
+    giturl: "",
+    weburl: "https://equityempowerment.org/",
+    description:
+      "SEEN Org is a website created for a non-profit organization to increase awareness about its mission and initiatives. The site was built to help visitors easily learn about the organization and its impact. I included an option for online donations to support their cause. Designed with a focus on simplicity and clarity, the website ensures an engaging experience for users while effectively communicating the organization’s goals.",
+  },
+
+  {
+    id: 6,
     title: "Music City Luggage Lounge",
     image: MusicCity,
     skills: ["WordPress", "Woocommerce", "CSS", "Paypal"],
     ss: Musicss,
     giturl: "",
     weburl: "https://musiccityluggage.com/",
-    description: "Excited to share my latest freelancing project: a comprehensive website and luggage booking system. This project showcases my branding, design, and web development skills using WordPress and WooCommerce to deliver an exceptional user experience. The website features a robust admin dashboard and a booking calendar to streamline operations and enhance usability for staff and customers. By leveraging WordPress's capabilities, I created a platform that is both visually appealing and highly functional."
-    
+    description:
+      "Excited to share my latest freelancing project: a comprehensive website and luggage booking system. This project showcases my branding, design, and web development skills using WordPress and WooCommerce to deliver an exceptional user experience. The website features a robust admin dashboard and a booking calendar to streamline operations and enhance usability for staff and customers. By leveraging WordPress's capabilities, I created a platform that is both visually appealing and highly functional.",
   },
   {
-    id: 4,
+    id: 7,
     title: "ProVisions Graphic",
     image: Provision,
-    skills: ["Startup","WordPress", "Woocommerce", "CSS","PHP", "Stripe"],
+    skills: ["Startup", "WordPress", "Woocommerce", "CSS", "PHP", "Stripe"],
     ss: Provisionss,
     giturl: "",
     weburl: "https://provisionsgraphic.com/",
-    description: "My Startup for offering monthly subscription packages for graphic designs. WordPress and WooCommerce-based project. This project will incorporate advanced functionalities such as membership and subscription features to facilitate seamless user experiences and recurring revenue models. I integrated Stripe as the payment gateway."
-    
+    description:
+      "My Startup for offering monthly subscription packages for graphic designs. WordPress and WooCommerce-based project. This project will incorporate advanced functionalities such as membership and subscription features to facilitate seamless user experiences and recurring revenue models. I integrated Stripe as the payment gateway.",
   },
   {
-    id: 5,
+    id: 8,
     title: "South Alba Sports",
     image: SouthAlba,
     skills: ["WordPress", "Woocommerce", "CSS", "Paypal"],
     ss: SouthAlbaSS,
     giturl: "",
     weburl: "https://southalbasports.com/",
-    description: "Project for a client based in the UK who organizes running events. Started to use Mid-journey ai generated images for projects from this project. The website was built on WordPress, and I designed all branding elements, including the logo. I customized the Kadence theme with custom CSS and used Elementor as the page builder. Additionally, I integrated PayPal as the payment gateway."
-    
+    description:
+      "Project for a client based in the UK who organizes running events. Started to use Mid-journey ai generated images for projects from this project. The website was built on WordPress, and I designed all branding elements, including the logo. I customized the Kadence theme with custom CSS and used Elementor as the page builder. Additionally, I integrated PayPal as the payment gateway.",
   },
 
   {
-    id: 6,
+    id: 9,
     title: "Lady Luck",
     image: Lady,
     skills: ["WordPress", "Woocommerce", "CSS", "Paypal"],
     ss: LadySS,
     giturl: "",
     weburl: "https://ladyluckaz.com/",
-    description: "Project for a client in the USA who specializes in selling cannabis pre-rolls. For this project, I adopted a distinctive dark theme to evoke a luxurious casino ambiance. The website was built on WordPress, with all branding elements, including the logo, meticulously designed by me"
-    
+    description:
+      "Project for a client in the USA who specializes in selling cannabis pre-rolls. For this project, I adopted a distinctive dark theme to evoke a luxurious casino ambiance. The website was built on WordPress, with all branding elements, including the logo, meticulously designed by me",
   },
 
   {
-    id: 7,
+    id: 10,
     title: "Riverside Accademy",
     image: Riverside,
     skills: ["WordPress", "Woocommerce", "CSS"],
     ss: RiversideSS,
     giturl: "",
     weburl: "https://riverside-academy.com/",
-    description: "Project for a client based in the UK who runs a baby photography business. The project involves creating a website to sell their online baby photography course. Wordpress-based project. Logo and all branding designed by me."
-    
+    description:
+      "Project for a client based in the UK who runs a baby photography business. The project involves creating a website to sell their online baby photography course. Wordpress-based project. Logo and all branding designed by me.",
   },
 
   {
-    id: 8,
+    id: 11,
     title: "My Portfolio",
     image: Portfolio,
-    skills: ["React", "Tailwind CSS", "CSS","Framer Motion" , "Figma", "Git"],
+    skills: ["React", "Tailwind CSS", "CSS", "Framer Motion", "Figma", "Git"],
     ss: PortfolioSS,
     giturl: "https://github.com/puliya2002/My-Portfolio-Website-.git",
     weburl: "https://pulinduvidmal.com/",
-    description: "Developed a portfolio website using React to showcase projects and skills. Designed an attractive UI/UX with Figma and enhanced the user experience with animations using Framer Motion. Leveraged React functional components, Hooks, and React Router to build a seamless and dynamic interface. Implemented image lazy loading to optimize performance and ensure fast loading times."
-    
+    description:
+      "Developed a portfolio website using React to showcase projects and skills. Designed an attractive UI/UX with Figma and enhanced the user experience with animations using Framer Motion. Leveraged React functional components, Hooks, and React Router to build a seamless and dynamic interface. Implemented image lazy loading to optimize performance and ensure fast loading times.",
   },
-
-
-
-  
-
- 
-  
-  
 ];
 
 export const NavLinks = [
@@ -125,7 +158,6 @@ export const NavLinks = [
     title: "Home",
     url: "hero",
     url2: "/",
-
   },
   {
     id: 2,
@@ -145,19 +177,13 @@ export const NavLinks = [
     url: "",
     url2: "contact",
   },
-
- 
-  
-  
 ];
-
 
 export const SkillList = [
   {
     id: 1,
     title: "JavaScriopt",
     url: "/",
-
   },
   {
     id: 2,
@@ -174,8 +200,4 @@ export const SkillList = [
     title: "HTML",
     url: "/contact",
   },
-
- 
-  
-  
 ];
